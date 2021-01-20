9824fcc325c0ce3b0ad54a80a930631ff7c0bad4
gastonm3112 <gastonm3112@gmail.com>
2021-01-20 08:43:57 -0300

    Orientacion dinamica creada

--- js/main.js
+++ js/main.js
@@ -10,7 +10,15 @@ const app = Vue.createApp({
     },
     computed: {
         setColor() {
-            return `background: linear-gradient(to right, ${this.firstColor}, ${this.secondColor});`;
+            if (this.orientation == 1) {
+                return `background: linear-gradient(to top, ${this.firstColor}, ${this.secondColor});`;
+            } else if (this.orientation == 2) {
+                return `background: linear-gradient(to right, ${this.firstColor}, ${this.secondColor});`;
+            } else if (this.orientation == 3) {
+                return `background: linear-gradient(to bottom, ${this.firstColor}, ${this.secondColor});`;
+            } else {
+                return `background: linear-gradient(to left, ${this.firstColor}, ${this.secondColor});`;
+            }
         }
     }
 });
